f694888d806e3bd105473a4a37b9f2d8b167e024
garywu007 <46955710+garywu007@users.noreply.github.com>
2020-09-12 23:24:26 +0800

    Add files via upload

--- socketServer.py
+++ socketServer.py
@@ -20,9 +20,10 @@ def message_received(client, server, message):
     server.send_message_to_all("Client(%d) said: %s" % (client['id'], message))
 
 
-PORT=443
+PORT=8989
 server = WebsocketServer(PORT)
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
 server.run_forever()
+print("started")
