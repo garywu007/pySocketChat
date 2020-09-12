17397f712536c31bcf37b3566cea03990108fade
garywu007 <46955710+garywu007@users.noreply.github.com>
2020-09-13 01:15:29 +0800

    Update socketServer.py

--- socketServer.py
+++ socketServer.py
@@ -1,31 +1,18 @@
-import os
-from websocket_server import WebsocketServer
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO
+import json
 
-# Called for every client connecting (after handshake)
-def new_client(client, server):
-    print("New client connected and was given id %d" % client['id'])
-    ## server.send_message_to_all("Hey all, a new client has joined us")
-    server.send_message(client, "you are connected and free to chat now!")
+app = Flask(__name__)
+socketio = SocketIO(app)
 
+@app.route("/")
+def index():
+  return render_template('index.html',)
 
-# Called for every client disconnecting
-def client_left(client, server):
-    print("Client(%d) disconnected" % client['id'])
+@socketio.on('send_message')
+def handle_source(json_data):
+  text = json_data['message'].encode('ascii', 'ignore')
+  socketio.emit('echo', {'echo': 'Server Says: '+text})
 
-
-# Called when a client sends a message
-def message_received(client, server, message):
-    if len(message) > 200:
-        message = message[:200]+'..'
-    print("Client(%d) said: %s" % (client['id'], message))
-    server.send_message_to_all("Client(%d) said: %s" % (client['id'], message))
-
-
-PORT=int(os.environ.get("PORT", 8989))
-print("==========starting=======" + str(PORT))
-server = WebsocketServer(PORT)
-server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
-server.set_fn_message_received(message_received)
-print("started")
-server.run_forever()
+if __name__ == "__main__":
+  socketio.run(app)
